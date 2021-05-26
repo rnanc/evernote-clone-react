22dfddb98dc53b6462efaad58042db18d4c1fc59
Renan <renanfigcarneiro@gmail.com>
2021-05-25 21:26:13 -0300

    feat: navbar component added

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className="green">
+      <div className="nav-wrapper">
+        <Link to="/" className="brand-logo">Notebook</Link>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><Link to="/favorites">Favorites</Link></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
