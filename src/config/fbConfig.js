dbcd3153203e392feee4200f7bdae4562b11b0fb
Renan <renanfigcarneiro@gmail.com>
2021-05-30 20:17:36 -0300

    feat: adicionado store de notas

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,6 +4,7 @@ import 'firebase/firestore'
 var firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
@@ -11,3 +12,5 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+export default firebase
