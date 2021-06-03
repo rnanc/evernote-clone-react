5da537a716f09867dc6dbbafdf798bc01b8b2662
Renan <renanfigcarneiro@gmail.com>
2021-06-03 11:56:48 -0300

    feat: media query on home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,9 +7,10 @@ import { useFirestoreConnect } from 'react-redux-firebase'
 const Home = () => {
   useFirestoreConnect([{collection: 'notes', orderBy:['createdAt', 'desc']}])
   const notes = useSelector(state => state.firestore.ordered.notes)
+  const media = window.matchMedia("(min-width: 768px)").matches ? 'row' : 'column'
   return (
     <div className="container">
-      <div className="row center-align">
+      <div className={`${media} center-align`}>
         <div className="col s7"><Form/></div>
         <div className="col s5"><NotesList notes={notes}/></div>
       </div>
