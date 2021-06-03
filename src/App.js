ab99fd7fdca9cd62db780d4c356d4834780ca162
Renan <renanfigcarneiro@gmail.com>
2021-06-03 11:14:35 -0300

    feat: novas rotas configuradas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './components/home/Home';
 import Navbar from './components/layout/Navbar'
+import Favorites from './components/notes/Favorites'
+import NoteDetail from './components/notes/NotesDetail'
+import EditForm from './components/notes/EditForm'
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route path="/favorites" component={Favorites}/>
+        <Route path="/note/:id" component={NoteDetail}/>
+        <Route path="/edit/:id" component={EditForm}/>
       </Switch>
     </BrowserRouter>
   );
