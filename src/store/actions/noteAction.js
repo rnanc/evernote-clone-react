85376f9fd2f2512f15bc47b3ef34dd0ecc1a62f1
Renan <renanfigcarneiro@gmail.com>
2021-06-03 11:15:17 -0300

    feat: adicionada action para o update

--- src/store/actions/noteAction.js
+++ src/store/actions/noteAction.js
@@ -44,3 +44,19 @@ export const toggleFav = (note) => {
     })
   }
 }
+
+export const updateNote = (note) => {
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore()
+    firestore.collection('notes').doc(note.id).update({
+      title: note.title,
+      content: note.content
+    })
+    .then(() => {
+      console.log('Success !')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+}
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { firebaseReducer } from "react-redux-firebase";
 import { firestoreReducer  } from "redux-firestore"
+import noteReducer from './noteReducer'
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
+  note: noteReducer,
   firebase: firebaseReducer,
   firestore: firestoreReducer
 })
